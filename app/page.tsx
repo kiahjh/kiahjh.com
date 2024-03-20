7c29e174d796be7e2a7fc11c3ba7e301b6ef2cd6
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-03-20 16:11:30 -0400

    added past projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import Noise from '../public/noise.svg';
-import ProfilePicture from '../public/me.jpg';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Noise from "../public/noise.svg";
+import ProfilePicture from "../public/me.jpg";
 
 export default function Home() {
   const stars: Array<{ size: number; left: number; top: number }> = [];
@@ -16,13 +16,13 @@ export default function Home() {
   }
 
   return (
-    <main className="min-h-screen bg-gradient-to-b from-pink-500 via-blue-500 to-white py-20 px-8 xs:px-12 relative flex flex-col">
+    <main className="min-h-screen bg-gradient-to-b from-pink-500 via-blue-500 to-white py-36 px-8 xs:px-12 relative flex flex-col">
       <div
         className="absolute w-full h-full bottom-0 left-0 bg-cover bg-center [filter:contrast(170%)_brightness(1000%)]"
         style={{
           backgroundImage: `linear-gradient(0deg, rgba(0,3,255,0.62), rgba(0,0,0,0)), url(${Noise.src})`,
         }}
-      ></div>
+      />
       <div className="w-full h-full left-0 top-0 absolute">
         {stars.map((star) => (
           <Star key={star.top} {...star} />
@@ -32,13 +32,16 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center flex-grow relative">
         <Image
           src={ProfilePicture}
-          alt={'picture of kiahjh'}
+          alt={"picture of kiahjh"}
           className="w-32 h-32 rounded-full object-cover object-center"
         />
         <h2 className="text-black font-light text-lg mt-8 mb-1">Hey there!</h2>
         <h1 className="text-4xl text-black font-extralight">
-          I&apos;m{' '}
-          <Link href="https://github.com/kiahjh" className="font-bold text-black">
+          I&apos;m{" "}
+          <Link
+            href="https://github.com/kiahjh"
+            className="font-bold text-black"
+          >
             @kiahjh
           </Link>
         </h1>
@@ -47,22 +50,45 @@ export default function Home() {
         </h3>
         <div className="mt-4 flex justify-center gap-8 flex-wrap">
           <Project
-            title={'Gertrude'}
+            title={"Gertrude"}
             description={`The best macOS internet safety and website blocker on the market.`}
-            githubUrl={'https://github.com/gertrude-app'}
-            url={'https://gertrude.app'}
+            githubUrl={"https://github.com/gertrude-app"}
+            url={"https://gertrude.app"}
           />
           <Project
-            title={'Friends Library'}
+            title={"Friends Library"}
             description={`Dedicated to the preservation and free distribution of early Quaker writings.`}
-            githubUrl={'https://github.com/friends-library'}
-            url={'https://friendslibrary.com'}
+            githubUrl={"https://github.com/friends-library"}
+            url={"https://friendslibrary.com"}
           />
           <Project
-            title={'The Ancient Path'}
+            title={"The Ancient Path"}
             description={`Writings and teachings of Jason R. Henderson.`}
-            githubUrl={'https://github.com/kiahjh/ancient-path'}
-            url={'https://hender.blog'}
+            githubUrl={"https://github.com/kiahjh/ancient-path"}
+            url={"https://hender.blog"}
+          />
+        </div>
+        <h3 className="text-black text-lg font-light mt-16 text-center">
+          Some other things I&apos;ve built:
+        </h3>
+        <div className="mt-4 flex justify-center gap-8 flex-wrap">
+          <Project
+            title={"next-bg-image"}
+            description={`Responsive, optimized, and accessible background images for Next.js. Made with @jaredh159.`}
+            githubUrl={"https://github.com/kiahjh/next-bg-image"}
+            url={"https://next-bg-image.kiahjh.com"}
+          />
+          <Project
+            title={"Canoe Creation Website"}
+            description={`A website for a local Christian canoeing/outdoor ministry.`}
+            githubUrl={"https://github.com/kiahjh/canoe-creation"}
+            url={"https://canoecreation.org"}
+          />
+          <Project
+            title={"Virtuoso Landscaping Website"}
+            description={`A website for a local landscaping company.`}
+            githubUrl={"https://github.com/kiahjh/virtuoso"}
+            url={"https://virtuosolandscapes.com"}
           />
         </div>
       </div>
@@ -110,7 +136,12 @@ interface ProjectProps {
   url: string;
 }
 
-const Project: React.FC<ProjectProps> = ({ title, description, githubUrl, url }) => {
+const Project: React.FC<ProjectProps> = ({
+  title,
+  description,
+  githubUrl,
+  url,
+}) => {
   return (
     <div className="border border-slate-100 bg-white/80 backdrop-blur-xl rounded-xl shadow-lg shadow-slate-300/30 flex flex-col">
       <div className="p-6 flex-grow">
